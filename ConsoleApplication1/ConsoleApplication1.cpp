cb3308c80280082524d5a73f1e36f1219e120df7
MdzJustin <justin.mendez.mena@est.una.ac.cr>
2023-10-25 18:50:49 -0600

    Fixed calculate points error

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int calcula_puntuacion(const vector<int>& puestos)
 {
-	const vector<int> puntos{ 10,8,6,5,4,3,2,1 };
+	const vector<int> puntos{ 10, 8, 6, 5, 4, 3, 2, 1 };
 
 	int suma = 0;
 	for (auto puesto : puestos)
@@ -27,10 +27,11 @@ void puntuacion(vector<string>& pilotos, vector<int>& puntos, string nombre_fich
 	}
 
 	string n_carreras;
-	getline(fich, n_carreras);//se lee la línea y se convierte su valor de texto a entero con atoi (ANSI to integer)
+	getline(fich, n_carreras);//se lee del documento la primera línea y se convierte su valor de texto a entero con atoi (ANSI to integer)
 	int num_carreras = atoi(n_carreras.c_str());
+
 	string n_pilotos;
-	getline(fich, n_pilotos);
+	getline(fich, n_pilotos);//baja a leer la segunda linea del archivo para obtener la cantidad de pilotos
 	int num_pilotos = atoi(n_pilotos.c_str());
 
 	pilotos.clear();  // Por si vienen con datos a la función
@@ -40,18 +41,19 @@ void puntuacion(vector<string>& pilotos, vector<int>& puntos, string nombre_fich
 	{
 		string piloto;
 		getline(fich, piloto);
-		pilotos.push_back(piloto);
+		pilotos.push_back(piloto);//agarra el piloto y lo sube al vector
 		vector<int> puestos;
 
 		int puesto;
 		string s_puestos;
+
 		getline(fich, s_puestos);
 		for (int j = 0; j < num_carreras; ++j)
 		{
 			int pos = 0;
-			while  (pos = s_puestos.find(" ")!= string::npos) {
+			while ((pos = s_puestos.find(" ") != string::npos) || (pos = s_puestos.find(";") != string::npos)) {//espacio entre carrera
 				if (pos == string::npos)
-					pos = s_puestos.find(";");
+					pos = s_puestos.find(";");//espacio para siguiente piloto
 				if (pos != string::npos) {
 					puesto = atoi(s_puestos.substr(0, pos).c_str());
 					s_puestos.erase(0, pos + 1);//se elimina de la cadena de puestos el dato extraído
